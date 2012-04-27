84d56c7493584dd7fc8da8b0101c1dc7f9e9423c
JP Villanueva <jpvi@ymail.com>
2012-04-26 20:17:09 -0700

    Finally got the partitioning right.

--- 06/lab6.c
+++ 06/lab6.c
@@ -8,32 +8,39 @@ int search_key_index;
 int* array;
 int array_size;
 int partitions;
+int element;
 
 void sequential_search(void*);
 
 int main(int argc, char** argv) {
     array_size = (int)pow(2, atoi(argv[1]));
     partitions = (int)pow(2, atoi(argv[2]));
-    int pivot = 1;
+    element = 0;
     int i;
-    int *thread_args = malloc(sizeof(int) * 2);
     int return_value;
     int search_key_counter;
+    int partition_size = array_size/partitions;
     pthread_t* search_threads = malloc(sizeof(pthread_t) * partitions);
     array = malloc(sizeof(int) * array_size);
     search_key = atoi(argv[3]);
 
+    printf("array size: %d\n", array_size);
+    printf("partitions: %d\n", partitions);
+
     srand(time(0));
 
     for(i = 0; i < array_size; i++) {
-        array[i] = rand() % array_size;
+        array[i] = rand();
+        printf("array[%d]: %d\n", i, array[i]);
     }
 
+    printf("\n");
+
     for(i = 0; i < partitions; i++) {
-        printf("%d to %d!\n", pivot, pivot+partitions);
-        thread_args[0] = pivot;
-        pivot += partitions;
-        thread_args[1] = pivot;
+        int *thread_args = malloc(sizeof(int) * 2);
+        thread_args[0] = element;
+        element += partition_size;
+        thread_args[1] = element;
         return_value = pthread_create(&search_threads[i], NULL, (void *)sequential_search, (void *)thread_args);
 
         if(return_value < 0) {
@@ -48,7 +55,7 @@ int main(int argc, char** argv) {
 
     for(i = 0; i < array_size; i++) {
         if(array[i] == search_key) {
-            printf("Search key found at element %d!\n", i);
+            printf("Final search key found at element %d!\n", i);
         }
     }
 
@@ -63,9 +70,9 @@ void sequential_search(void* args) {
     int end = search_args[1];
     int i;
 
-    printf("%d to %d\n", start, end);
-
     for(i = start; i < end; i++) {
+        printf("element %d: %d\n", i, array[i]);
+
         if(array[i] == search_key) {
             printf("Search key found at element %d!\n", i);
             search_key_index = i;
