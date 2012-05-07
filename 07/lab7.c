b652a53e8ae4b8cedcbf8c64e5724acf0c627a4c
JP Villanueva <jpvi@ymail.com>
2012-05-07 16:36:12 -0700

    Finished Linux implementation.

--- 07/lab7.c
+++ 07/lab7.c
@@ -1,29 +1,27 @@
 #include <math.h>
+#include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <pthread.h>
-#include <limits.h>
+#include <time.h>
+#include <unistd.h>
 
 int array_size;
 int partitions;
 int* array;
-int* min;
 
-void selection_sort(void*);
+void insertion_sort(void*);
 void final_merge();
-int get_next();
 
 int main(int argc, char** argv) {
     int i;
     int return_value;
-    long seconds;
-    long milliseconds;
-    long time_elapsed;
-    struct timeval start;
-    struct timeval end;
+    float seconds;
+    float nanoseconds;
+    float time_elapsed;
+    struct timespec start;
+    struct timespec end;
     array_size = (int)pow(2, atoi(argv[1]));
     array = malloc(sizeof(int) * array_size);
-    min = malloc(sizeof(int) * partitions);
     partitions = (int)pow(2, atoi(argv[2]));
     pthread_t* threads = malloc(sizeof(pthread_t) * partitions);
 
@@ -32,19 +30,19 @@ int main(int argc, char** argv) {
     printf("partition size: %d\n", array_size/partitions);
     printf("\n");
 
-    /* Start timing the search. */
-    //gettimeofday(&start, NULL);
-
     /* Entropy for RNG. */
     srand(time(0));
 
     for(i = 0; i < array_size; i++) {
         /* Creates random numbers to insert into the array that are between 0-2^n. */
-        array[i] = rand() % array_size;
+        array[i] = (unsigned int)rand() % array_size;
     }
 
+    /* Start timing the search. */
+    clock_gettime(CLOCK_REALTIME, &start);
+
     for(i = 0; i < partitions; i++) {
-        return_value = pthread_create(&threads[i], NULL, (void*)selection_sort, (void*)i);
+        return_value = pthread_create(&threads[i], NULL, (void*)insertion_sort, (void*)i);
 
         if(return_value < 0) {
             perror("Cannot create a thread");
@@ -56,88 +54,64 @@ int main(int argc, char** argv) {
         pthread_join(threads[i], NULL);
     }
 
-    gettimeofday(&end, NULL);
-
     final_merge();
 
-    seconds = end.tv_sec - start.tv_sec;
-    milliseconds = end.tv_usec - start.tv_usec;
-    time_elapsed = (((seconds * 1000) + milliseconds)/1000.0) + 0.5;
+    clock_gettime(CLOCK_REALTIME, &end);
 
-    printf("Time elapsed: %ld ms\n", time_elapsed);
+    seconds = end.tv_sec - start.tv_sec;
+    nanoseconds = end.tv_nsec - start.tv_nsec;
+    time_elapsed = (seconds + nanoseconds)/100000.0;
+
+    printf("Time elapsed: %lf ns\n", time_elapsed);
 
     free(array);
-    free(min);
 
     return 0;
 }
 
-void selection_sort(void *arg) {
+void insertion_sort(void *arg) {
     int index = (int)arg;
     int start = (array_size/partitions) * index;
     int end = start + (array_size/partitions) - 1;
-    int i;
-    int j;
-    int minimum;
+    int i, j, value;
 
-    for(i = start; i < end; i++) {
-        minimum = i;
+    for(i = start; i <= end; i++) {
+        value = array[i];
 
-        for(j = i+1; j < end; j++) {
-            if(array[j] < array[minimum]) {
-                minimum = j;
-            }
+        for(j = i; j > 0 && value < array[j-1]; j--) {
+            array[j] = array[j-1];
         }
 
-        if(minimum != i) {
-            int temp;
-
-            temp = array[minimum];
-            array[minimum] = array[i];
-            array[i] = temp;
-        }
+        array[j] = value;
     }
+
+    /*
+    for(i = start; i <=end; i++) {
+        printf("array[%d] = %d\n", i, array[i]);
+    }
+
+    printf("\n");
+    */
 }
 
 void final_merge() {
     int i;
     int j;
-    int start;
-    int end;
-
-    for(i = 0; i < partitions; i++) {
-        start = (array_size/partitions) * i;
-        end = start + (array_size/partitions);
-
-        printf("partition: %d:\n", i);
-
-        for(j = start; j < end; j++) {
-            printf("array[%d] = %d\n", j, array[j]);
-        }
-
-        printf("\n\n");
-    }
+    int value;
 
     for(i = 0; i < array_size; i++) {
-        int current = get_next();
+        value = array[i];
 
-        printf("array element = %d\n", current);
-    }
-}
-
-int get_next() {
-    int i;
-    int smallest;
-    int minval = 0;
-
-    for(i = 1; i < partitions; i++) {
-        if(array[min[i]] > array[min[minval]]) {
-            minval = i;
+        for(j = i; j > 0 && value < array[j-1]; j--) {
+            array[j] = array[j-1];
         }
+
+        array[j] = value;
     }
 
-    smallest = array[min[minval]];
-    min[minval]++;
-
-    return smallest;
+    /*
+    for(i = 0; i < array_size; i++) {
+        printf("%d\n", array[i]);
+    }
+    */
 }
